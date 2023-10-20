706c270b14d75efcc9e0e415a56e34f351101b5b
Scott Fasone <scott181182@yahoo.com>
2023-10-20 00:33:04 +0000

    feat(frontend): made home page into a component to allow context referencing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,71 +1,16 @@
 "use client";
 
-import { Layout, Skeleton, Typography } from "antd";
-import dynamic from "next/dynamic";
-import { useState } from "react";
-
+import { HomeView } from "@/components/HomeView";
 import { DashboardProvider } from "@/contexts/DashboardContext";
 import { EnumProvider } from "@/contexts/EnumContext";
-import { FileBrowser } from "@/components/FileBrowser";
-import { RelationBrowser } from "@/components/RelationBrowser";
-
-// Load map dynamically to avoid Next13 prerender errors.
-const Map = dynamic(() => import("@/components/map").then((mod) => mod.Map), {
-    loading: () => <Skeleton/>,
-    ssr: false
-});
 
 
 
 export default function HomePage() {
-    const [fileBrowserCollapsed, setFileBrowserCollapsed] = useState(true);
-    const [relationBrowserCollapsed, setRelationBrowserCollapsed] = useState(true);
-
-
-
     return (
         <EnumProvider>
             <DashboardProvider>
-                <Layout hasSider className="h-full">
-                    <Layout.Sider
-                        collapsible
-                        collapsed={fileBrowserCollapsed}
-                        onCollapse={setFileBrowserCollapsed}
-                        theme="light"
-                        width="35%"
-                    >
-                        {fileBrowserCollapsed ?
-                            <Typography.Title
-                                level={2}
-                                className="rotate-90 whitespace-nowrap mt-10"
-                            >
-                                    File Browser
-                            </Typography.Title> :
-                            <FileBrowser/>
-                        }
-                    </Layout.Sider>
-                    <Layout.Content className="relative">
-                        <Map/>
-                    </Layout.Content>
-                    <Layout.Sider
-                        collapsible
-                        collapsed={relationBrowserCollapsed}
-                        onCollapse={setRelationBrowserCollapsed}
-                        theme="light"
-                        width="20%"
-                        reverseArrow
-                    >
-                        {relationBrowserCollapsed ?
-                            <Typography.Title
-                                level={2}
-                                className="rotate-90 whitespace-nowrap mt-10"
-                            >
-                                Relation Browser
-                            </Typography.Title> :
-                            <RelationBrowser/>
-                        }
-                    </Layout.Sider>
-                </Layout>
+                <HomeView/>
             </DashboardProvider>
         </EnumProvider>
     );
---
+++ frontend/src/components/HomeView.tsx
@@ -0,0 +1,74 @@
+import { Layout, Skeleton, Typography } from "antd";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+
+import { FileBrowser } from "@/components/FileBrowser";
+import { RelationBrowser } from "@/components/RelationBrowser";
+import { useDashboardState } from "@/contexts/DashboardContext";
+
+
+
+// Load map dynamically to avoid Next13 prerender errors.
+const Map = dynamic(() => import("@/components/map").then((mod) => mod.Map), {
+    loading: () => <Skeleton/>,
+    ssr: false
+});
+
+
+
+export function HomeView() {
+    const { selectedFiles } = useDashboardState();
+
+    const [fileBrowserCollapsed, setFileBrowserCollapsed] = useState(true);
+    const [relationBrowserCollapsed, setRelationBrowserCollapsed] = useState(true);
+
+    useEffect(() => {
+        if(selectedFiles.length > 0) {
+            setFileBrowserCollapsed(false);
+        }
+    }, [selectedFiles]);
+
+
+    return (
+        <Layout hasSider className="h-full">
+            <Layout.Sider
+                collapsible
+                collapsed={fileBrowserCollapsed}
+                onCollapse={setFileBrowserCollapsed}
+                theme="light"
+                width="35%"
+            >
+                {fileBrowserCollapsed ?
+                    <Typography.Title
+                        level={2}
+                        className="rotate-90 whitespace-nowrap mt-10"
+                    >
+                                    File Browser
+                    </Typography.Title> :
+                    <FileBrowser/>
+                }
+            </Layout.Sider>
+            <Layout.Content className="relative">
+                <Map/>
+            </Layout.Content>
+            <Layout.Sider
+                collapsible
+                collapsed={relationBrowserCollapsed}
+                onCollapse={setRelationBrowserCollapsed}
+                theme="light"
+                width="20%"
+                reverseArrow
+            >
+                {relationBrowserCollapsed ?
+                    <Typography.Title
+                        level={2}
+                        className="rotate-90 whitespace-nowrap mt-10"
+                    >
+                                Relation Browser
+                    </Typography.Title> :
+                    <RelationBrowser/>
+                }
+            </Layout.Sider>
+        </Layout>
+    );
+}
