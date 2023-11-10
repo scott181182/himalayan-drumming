e19f6c5581e15fe8370fd324388cbeca771693f9
Scott Fasone <scott181182@yahoo.com>
2023-11-10 15:35:31 +0000

    feat(frontend): added ability to add file references, as well as open them in new tabs

--- frontend/src/components/FileBrowser/hooks.tsx
+++ frontend/src/components/FileBrowser/hooks.tsx
@@ -1,9 +1,11 @@
-import { useDashboardDispatch } from "@/contexts/DashboardContext";
-import { CreateFolderDocument } from "@/generated/graphql";
 import { FileImageOutlined } from "@ant-design/icons";
 import { useApolloClient } from "@apollo/client";
 import { Input, Modal, Upload } from "antd";
+import type { ReactNode} from "react";
 import { useCallback, useState } from "react";
+
+import { useDashboardDispatch } from "@/contexts/DashboardContext";
+import { CreateFileReferenceDocument, CreateFolderDocument } from "@/generated/graphql";
 
 
 
@@ -36,8 +38,8 @@ export function useCreateFolderModal() {
         }).then(() => {
             onCancel();
             return refetchDashboard();
-        })
-    }, [apolloClient, folderName]);
+        });
+    }, [apolloClient, folderName, onCancel, parentId, refetchDashboard]);
 
     const openCreateFolderModal = useCallback((parentId: string) => {
         setParentId(parentId);
@@ -61,7 +63,7 @@ export function useCreateFolderModal() {
             onChange={(ev) => setFolderName(ev.target.value)}
             placeholder="Folder Name"
         />
-    </Modal>
+    </Modal>;
 
     return {
         openCreateFolderModal,
@@ -96,7 +98,9 @@ export function useUploadFileModal() {
         okButtonProps={{
             disabled: !parentId
         }}
-        footer={(_, { CancelBtn }) => <CancelBtn/>}
+        // Casting here because the Ant Design type isn't up-to-date with this footer render function.
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        footer={((_: unknown, { CancelBtn }: { CancelBtn: any }) => <CancelBtn/>) as unknown as ReactNode}
     >
         <Upload.Dragger
             method="POST"
@@ -116,11 +120,92 @@ export function useUploadFileModal() {
             </p>
             <p className="ant-upload-text">Click or drag file to this area to upload</p>
         </Upload.Dragger>
-    </Modal>
+    </Modal>;
 
     return {
         openUploadFileModal,
         closeUploadFileModal: onCancel,
         uploadFileModalContent
+    };
+}
+
+
+
+export function useAddReferenceModal() {
+    const apolloClient = useApolloClient();
+    const { refetchDashboard } = useDashboardDispatch();
+
+    const [open, setOpen] = useState(false);
+    const [parentId, setParentId] = useState<string | undefined>();
+    const [fileName, setFileName] = useState("");
+    const [fileUrl, setFileUrl] = useState("");
+
+    const onCancel = useCallback(() => {
+        setOpen(false);
+        setFileName("");
+        setFileUrl("");
+    }, [setOpen]);
+
+    const onSubmit = useCallback(() => {
+        if(!parentId) {
+            console.warn("No parentId set, cannot submit file reference");
+            return;
+        }
+        if(!fileName) {
+            console.warn("No fileName set, cannot submit file reference");
+            return;
+        }
+        if(!fileUrl) {
+            console.warn("No fileUrl set, cannot submit file reference");
+            return;
+        }
+        apolloClient.mutate({
+            mutation: CreateFileReferenceDocument,
+            variables: {
+                parentId,
+                name: fileName,
+                url: fileUrl
+            }
+        }).then(() => {
+            onCancel();
+            return refetchDashboard();
+        });
+    }, [apolloClient, fileName, fileUrl, onCancel, parentId, refetchDashboard]);
+
+    const openAddReferenceModal = useCallback((parentId: string) => {
+        setParentId(parentId);
+        setOpen(true);
+    }, [setParentId, setOpen]);
+
+
+
+    const addReferenceModalContent = <Modal
+        open={open}
+        closable
+        onCancel={onCancel}
+        okText="Add File Reference"
+        onOk={onSubmit}
+        okButtonProps={{
+            disabled: !parentId
+        }}
+        bodyStyle={{ paddingRight: "1.5rem" }}
+    >
+        <Input
+            value={fileName}
+            onChange={(ev) => setFileName(ev.target.value)}
+            placeholder="Reference Name"
+            className="mb-2"
+        />
+        <Input
+            value={fileUrl}
+            onChange={(ev) => setFileUrl(ev.target.value)}
+            placeholder="Reference URL"
+        />
+    </Modal>;
+
+    return {
+        openAddReferenceModal,
+        closeCreateFolderModal: onCancel,
+        addReferenceModalContent
     };
 }
