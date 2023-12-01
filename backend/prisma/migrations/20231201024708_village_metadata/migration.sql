107b380b9f887223280ca03a9677602735f1d2b2
Scott Fasone <scott181182@yahoo.com>
2023-12-01 02:47:31 +0000

    feat(backend): added village metadata fields to prisma schema

---
+++ backend/prisma/migrations/20231201024708_village_metadata/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Village" ADD COLUMN "divinities" TEXT;
+ALTER TABLE "Village" ADD COLUMN "notes" TEXT;
+ALTER TABLE "Village" ADD COLUMN "rituals" TEXT;
+ALTER TABLE "Village" ADD COLUMN "temples" TEXT;
