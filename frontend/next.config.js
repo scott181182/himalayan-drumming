d95559d0b8652d1934602fed1adad1b5daa446e9
Scott Fasone <scott181182@yahoo.com>
2025-10-28 13:24:29 -0400

    feat: updated backend to use Postgres

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "standalone",
+    rewrites: async () => [
+        {
+            source: "/api/:path*",
+            destination: "http://localhost:3001/:path*"
+        }
+    ]
+};
 
 module.exports = nextConfig;
