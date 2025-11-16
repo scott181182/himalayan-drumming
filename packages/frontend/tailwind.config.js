432466c0ca0ea516e36a0b6ee5c3c647353c32a0
Scott Fasone <scott181182@yahoo.com>
2025-11-16 21:57:14 +0000

    feat(frontend): added centered container to tailwind config

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
@@ -10,11 +9,7 @@ module.exports = {
     },
     theme: {
         extend: {
-            // backgroundImage: {
-            //     "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-            //     "gradient-conic":
-            //         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            // },
+            container: { center: true }
         },
     },
     plugins: [],
