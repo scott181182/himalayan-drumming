e13991e172c8bb82c582944ff0b7f32885033d65
Scott Fasone <scott181182@yahoo.com>
2025-11-16 21:59:32 +0000

    feat(frontend): migrated from msal to better-auth and added pages for login and signup

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -3,8 +3,14 @@ const nextConfig = {
     output: "standalone",
     rewrites: async () => [
         {
+            // Keep auth endpoints in NextJS.
+            source: "/api/auth/:path*",
+            destination: "/api/auth/:path*",
+        },
+        {
+            // Redirect other API endpoints to backend server.
             source: "/api/:path*",
-            destination: "http://localhost:3001/:path*"
+            destination: "http://localhost:3001/api/:path*"
         }
     ]
 };
--- packages/frontend/src/lib/apollo.ts
+++ packages/frontend/src/lib/apollo.ts
@@ -2,16 +2,14 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 
 
-export function createApolloClient(token: string) {
+export function createApolloClient() {
     const rootEndpoint = process.env.NEXT_PUBLIC_API_ROOT_ENDPOINT || "/api";
 
     return new ApolloClient({
         cache: new InMemoryCache(),
         link: new HttpLink({
             uri: rootEndpoint + "/graphql",
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
+            credentials: "same-origin"
         })
     });
 }
