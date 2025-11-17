4ea483a78d37550a7bd56111302089caa37c219f
Scott Fasone <scott181182@yahoo.com>
2025-11-17 02:53:27 +0000

    feat(database): made database package work better with non-Rust query engine

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,1 +1,15 @@
-export * from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import type { PoolConfig } from "pg";
+
+import { PrismaClient } from "./generated/prisma/client.js";
+
+
+
+export function createPrismaClient(poolConfig: PoolConfig) {
+    const adapter = new PrismaPg(poolConfig);
+    const client = new PrismaClient({ adapter });
+
+    return client;
+}
+
+export * from "./generated/prisma/client.js";
