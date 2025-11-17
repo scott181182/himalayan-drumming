80386210cd8398bb0b9f0dd9db7895d877be01e2
Scott Fasone <scott181182@yahoo.com>
2025-11-17 04:32:08 +0000

    revert: undoing earlier fixes that may not have had an impact

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,17 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require("node:path");
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
     serverExternalPackages: [
         "himalayan-drumming-research-database"
-    ],
-    turbopack: {
-        root: path.resolve(__dirname, "..", "..")
-    },
+    ]
 };
 
 module.exports = nextConfig;
