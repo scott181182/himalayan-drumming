12e9e1b5308bd3748b6262baeba7de17abf12545
Scott Fasone <scott181182@yahoo.com>
2025-11-17 03:49:36 +0000

    chore(cicd): manually specifying monorepo root for turbopack

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,9 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("node:path");
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
     serverExternalPackages: [
         "himalayan-drumming-research-database"
     ],
+    turbopack: {
+        root: path.resolve(__dirname, "..", "..")
+    },
 };
 
 module.exports = nextConfig;
